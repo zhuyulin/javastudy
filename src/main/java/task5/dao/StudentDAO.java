91b40f33f650ae6b45f1d97315400ce7f9be9703
Yuleen <zhuyulin861@qq.com>
2016-12-18 12:55:11 +0800

    update1218——task5

--- src/main/java/task5/dao/StudentDAO.java
+++ src/main/java/task5/dao/StudentDAO.java
@@ -6,10 +6,6 @@ import java.util.List;
  * Created by Yuleen on 2016/12/17.
  */
 public class StudentDAO extends BaseDAO {
-    public Integer countStudent(){
-        Integer count = getJdbcTemplate().queryForObject("select count(*) from students",Integer.class);
-        return count;
-    }
     public List<StudentDO> findByName(String name){
         List<StudentDO> studentDOList = getJdbcTemplate().query("select * from students where sname like ?"
                 ,new StudentDO(),new Object[]{"%"+name+"%"});
